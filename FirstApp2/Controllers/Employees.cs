75f5f96a0c9730aba56cc65858f5b9230fc29781
mahmoud <mahmoud995boghdady@gmail.com>
2024-10-14 21:14:42 +0300

    test

---
+++ FirstApp2/Controllers/Employees.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstApp2.Models;
+
+namespace FirstApp2.Controllers
+{
+    public class Employees : Controller
+    {
+        List<EmployeeModel> LstEmployees;
+        void createEmployeesList()
+        {
+            LstEmployees = new List<EmployeeModel>();
+            EmployeeModel model = new EmployeeModel();
+            model.Id = 1;
+            model.Name = "Iron Man";
+            model.Title = "Innovation";
+            model.Description = "Description";
+            model.ImageName = "1.jpg";
+            LstEmployees.Add(model);
+
+            model = new EmployeeModel();
+            model.Id = 2;
+            model.Name = "BatMan";
+            model.Title = "Developer";
+            model.Description = "Description";
+            model.ImageName = "2.jpg";
+            LstEmployees.Add(model);
+
+            model = new EmployeeModel();
+            model.Id = 3;
+            model.Name = "Joie Doe";
+            model.Title = "Actross";
+            model.Description = "Description";
+            model.ImageName = "3.jpg";
+            LstEmployees.Add(model);
+        }
+        public IActionResult List()
+        {
+            createEmployeesList();
+            return View(LstEmployees);
+        }
+        public IActionResult Details(int id, string name)
+        {
+            createEmployeesList();
+            var myEmployee = LstEmployees.Where(a => a.Id == id).FirstOrDefault();
+            return View(myEmployee);
+        }
+
+    }
+}
---
+++ FirstApp2/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+
+namespace FirstApp2
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			builder.Services.AddControllersWithViews();
+			var app = builder.Build();
+
+			if(!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseRouting();
+			app.UseAuthorization();
+
+			
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+				endpoints.MapControllerRoute(
+				name: "ali",
+				pattern: "ali/{controller=Home}/{action=Index}/{id?}");
+			
+		});
+
+
+
+			app.MapGet("/", () => "Hello World!");
+
+			app.Run();
+		}
+	}
+}
